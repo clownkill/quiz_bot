eeaccd8ea6f1ab05ea208063473947f599755568
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-04-13 22:50:32 +0300

    add creating quiz

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-def get_question_file(file):
-    with open(file, 'r', encoding='koi8-r') as file:
-        file_questions = file.read()
+from itertools import zip_longest
 
-    print(file_questions)
+
+def chunk(lst):
+    i_ = iter(lst)
+    return list(zip_longest(i_, i_))
+
+
+def create_quiz(file):
+    with open(file, 'r', encoding='KOI8-R') as f:
+        text = f.read()
+
+    sep_text = [part for part in text.split('\n\n')]
+
+    answers_and_questions = list(filter(lambda elem: 'Вопрос' in elem or 'Ответ' in elem, sep_text))
+    quiz_desc = [
+        {
+            'question': pair[0].split('\n', 1)[1],
+            'answer': pair[1].split('\n', 1)[1]
+        }
+        for pair in chunk(answers_and_questions)
+    ]
+
+    return quiz_desc
 
 
 def main():
     file = 'temp/1vs1200.txt'
-    get_question_file(file)
+    quiz = create_quiz(file)
 
 
 if __name__ == '__main__':
